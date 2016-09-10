0d921957c4e07bc9a4d6bdc6c004f29aaf003200
shivkanthb <shivkanthb@gmail.com>
2016-09-09 23:44:58 -0700

    updated views

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160910060935) do
+ActiveRecord::Schema.define(version: 20160910062534) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
